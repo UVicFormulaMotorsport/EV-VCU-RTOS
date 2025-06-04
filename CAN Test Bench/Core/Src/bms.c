5a691e367d8d57c69e1aa2683a87a0854cf3f4df
quaziheider <63382559+justquazi@users.noreply.github.com>
2025-06-03 23:54:55 -0400

    Updating new code into correct branch (origin/quazi)

--- CAN Test Bench/Core/Src/bms.c
+++ CAN Test Bench/Core/Src/bms.c
@@ -8,6 +8,68 @@
 #include "tim.h"
 #include "dash.h"
 
+//two IDs for bms can message
+// 0x6B0
+// 0x6B1
+
+uint16_t packCurrent = 0; // logged
+uint16_t packVoltage = 0; // logged
+uint16_t stateOfCharge = 0; // used for toggling regen braking / low charge warning
+uint16_t relayState = 0;
+uint16_t msg1corrupt = 0;
+
+uint16_t packDCL = 0;
+uint16_t lowTemp = 0;
+uint16_t highTemp = 0;
+uint16_t msg2corrupt = 0;
+// extern uint16_t (var name) to call outside this file
+
+
+void BMS_msg1(uv_CAN_msg* msg){ // msg is raw CAN msg, gets processed in voltageCANdata
+	// msg1 can handle current, voltage, charge, relay, checksum (corruption)
+	packCurrent = (msg->data[0]<<8 | msg->data[1]); // x 0.1A
+	packVoltage = (msg->data[2]<<8 | msg->data[3]); // x 0.1V
+
+	stateOfCharge = (msg->data[4])*2; // x2; this is an int
+
+	relayState = (msg->data[5]<<8 | msg ->data[6]);
+	msg1corrupt = (msg->data[7]);
+	if (stateOfCharge < 20) {
+		// flash low battery warning to screen
+	}
+	if (stateOfCharge >= 95) {
+		// disable regenerative breaking
+	}
+	// in all other cases battery should be functioning as normal
+
+
+
+	// would make sense for voltageCANdata to turn into float
+	//log voltage data to where, maybe setting global variable
+}
+
+void BMS_msg2(uv_CAN_msg* msg){
+	// msg2 can handle DCL (Max current output), tempurature, checksum
+	packDCL = (msg->data[0]<<8 | msg->data[3]);
+	lowTemp = (msg->data[4]); // celsius
+	highTemp = (msg->data[5]); // celsius
+	msg2corrupt = (msg->data[7]);
+
+	 if (lowTemp <= -5 ) {
+		// warning - temp is too low
+		// stop operability?
+
+	 }
+	 else if (lowTemp > -5 && highTemp < 55){
+//		 normal operating tempurature range
+//		 drive as normal
+		 if (temp >= 55.0) {
+//			 flash warning - approaching max temp
+//			 maybe enable cooling fan/devices here
+		 }
+	 }
+}
+
 void BMS_Init(void* args){
 	uv_init_task_args* params = (uv_init_task_args*) args;
 
@@ -19,12 +81,18 @@ void BMS_Init(void* args){
 			//OOPS
 	}
 
-
-
+	insertCANMessageHandler(251, BMS_logVoltage);
+	insertCANMessageHandler(252, BMS_stateOfCharge);
+	insertCANMessageHandler(253, BMS_tempurature);
 
 		//Kill yourself
 	vTaskSuspend(params->meta_task_handle);
 }
+
+
+
+
+
 
 
 
